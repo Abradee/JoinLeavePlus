4745d8d7af59d9ea403cf756d3aa900a9f481dfe
abradee <cptnspacecat@outlook.com>
2026-06-29 19:12:17 -0400

    Fixed leave messages not showing
    how did i miss that lmao

--- src/main/java/me/abradee/joinLeavePlus/Listeners/MainListener.java
+++ src/main/java/me/abradee/joinLeavePlus/Listeners/MainListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MainListener implements Listener {
 
@@ -13,15 +14,21 @@ public class MainListener implements Listener {
         String name = player.getName();
 
         if (!player.hasPlayedBefore()) {
-            // First time join checklist
             FirstJoinMessageListener.handle(e, name);
             FirstJoinSoundListener.handle(player);
             FirstJoinTitleListener.handle(player, name);
         } else {
-            // Regular join checklist
             JoinMessageListener.handle(e, name);
             JoinSoundListener.handle(player);
             JoinTitleListener.handle(player, name);
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        String name = player.getName();
+
+        LeaveMessageListener.handle(e, name);
     }
 }
