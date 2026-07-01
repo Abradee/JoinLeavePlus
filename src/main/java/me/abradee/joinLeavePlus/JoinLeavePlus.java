f60c14cfda41140f994f355a17d2d16edf3d6864
Abradee <cptnspacecat@outlook.com>
2026-07-01 11:44:18 -0400

    Added a version checker
    this tells you if you are up to date and if not tells you where to get it.

--- src/main/java/me/abradee/joinLeavePlus/JoinLeavePlus.java
+++ src/main/java/me/abradee/joinLeavePlus/JoinLeavePlus.java
@@ -3,6 +3,10 @@ package me.abradee.joinLeavePlus;
 import me.abradee.joinLeavePlus.Listeners.*;
 import me.abradee.joinLeavePlus.Commands.AboutCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import de.clickism.modrinthupdatechecker.ModrinthUpdateChecker;
+
+// includes other code from clickism's ModrinthUpdateChecker
+// check LICENSE or check Github
 
 public final class JoinLeavePlus extends JavaPlugin {
 
@@ -14,6 +18,23 @@ public final class JoinLeavePlus extends JavaPlugin {
         getCommand("about-joinleaveplus").setExecutor(new AboutCommand());
         getLogger().info("The plugin has started.");
         getLogger().info("Feel free to donate through https://patreon.com/abradee");
+        new ModrinthUpdateChecker("joinleaveplus", "paper", null)
+                .checkVersion(latestVersion -> {
+                    String currentVersion = getDescription().getVersion();
+
+                    if (currentVersion.contains("-SNAPSHOT")) {
+                        getLogger().warning("Running a development build (" + currentVersion + "). Skipping update check.");
+                        return;
+                    }
+
+                    if (currentVersion.equals(latestVersion)) { // later change this to just check if the version is lower or higher than the posted version due to unreleased versions saying they're old.
+                        getLogger().info("You are running the latest version!");
+                    } else {
+                        getLogger().warning("A new update is available: v" + latestVersion);
+                        getLogger().warning("Your current version: v" + currentVersion);
+                        getLogger().warning("Download it here: https://modrinth.com/plugin/joinleaveplus");
+                    }
+                });
     }
 
     @Override
