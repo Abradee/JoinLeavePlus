69070828054ea878712c0ccee086b70a856db211
abradee <cptnspacecat@outlook.com>
2026-07-01 17:44:40 -0400

    adding stuffed based on docs

--- src/main/java/me/abradee/joinLeavePlus/Listeners/FirstJoin/FirstJoinBookListener.java
+++ src/main/java/me/abradee/joinLeavePlus/Listeners/FirstJoin/FirstJoinBookListener.java
@@ -1,5 +1,6 @@
 package me.abradee.joinLeavePlus.Listeners.FirstJoin;
 
+import net.kyori.adventure.title.Title;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerJoinEvent;
 import net.kyori.adventure.inventory.Book;
@@ -10,5 +11,6 @@ public class FirstJoinBookListener {
         String firstJoinBookAuthor = JavaPlugin.getProvidingPlugin(FirstJoinMessageListener.class).getConfig().getString("book-author");
         java.util.List<String> firstJoinBookPages = JavaPlugin.getProvidingPlugin(FirstJoinMessageListener.class).getConfig().getStringList("book-pages");
 
+        Book.Builder(firstJoinBookTitle Title, firstJoinBookAuthor Author);
     }
 }
