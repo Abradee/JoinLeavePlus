2b430330eb03d6987d83513516f1e8ae22f6ebfa
abradee <cptnspacecat@outlook.com>
2026-07-16 10:43:15 -0500

    added some license text in code

--- src/main/java/me/abradee/joinLeavePlus/JoinLeavePlus.java
+++ src/main/java/me/abradee/joinLeavePlus/JoinLeavePlus.java
@@ -1,3 +1,13 @@
+/*
+    (c) 2026 JoinLeavePlus under the GPLv3 License
+    (c) 2026 Abradee
+    Check LICENSE for more license information.
+    Check CONTRIBUTING.md for contributing information.
+    =========================================================
+    includes other code from clickism's ModrinthUpdateChecker
+    check LICENSE or check on GitHub
+*/
+
 package me.abradee.joinLeavePlus;
 
 import me.abradee.joinLeavePlus.Listeners.*;
@@ -5,8 +15,6 @@ import me.abradee.joinLeavePlus.Commands.AboutCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 import de.clickism.modrinthupdatechecker.ModrinthUpdateChecker;
 
-// includes other code from clickism's ModrinthUpdateChecker
-// check LICENSE or check on GitHub
 
 public final class JoinLeavePlus extends JavaPlugin {
 
--- src/main/java/me/abradee/joinLeavePlus/Listeners/FirstJoin/FirstJoinSoundListener.java
+++ src/main/java/me/abradee/joinLeavePlus/Listeners/FirstJoin/FirstJoinSoundListener.java
@@ -1,3 +1,13 @@
+/*
+    (c) 2026 JoinLeavePlus under the GPLv3 License
+    (c) 2026 Abradee
+    Check LICENSE for more license information.
+    Check CONTRIBUTING.md for contributing information.
+    =========================================================
+    includes other code from clickism's ModrinthUpdateChecker
+    check LICENSE or check on GitHub
+*/
+
 package me.abradee.joinLeavePlus.Listeners.FirstJoin;
 
 import me.abradee.joinLeavePlus.Listeners.Join.JoinSoundListener;
